f3c1a62e5dfe6619c55580cff3653c9095e50367
Ned Konz <ned@metamagix.tech>
2026-01-17 12:27:04 -0800

    Increase hex buffer size to 128K for handling 32K ROMs

--- src/usb_cdc.c
+++ src/usb_cdc.c
@@ -27,7 +27,7 @@ static uint32_t cmd_pos = 0;
 static bool in_hex_mode = false;
 
 // HEX data buffer
-#define HEX_BUFFER_SIZE 32768
+#define HEX_BUFFER_SIZE (131072)
 static char hex_buffer[HEX_BUFFER_SIZE];
 static uint32_t hex_pos = 0;
 
